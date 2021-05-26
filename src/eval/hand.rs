3c243716a65f91cd4e32c42c3f31ef658c161db3
Itay <itaysadeh6@gmail.com>
2021-05-26 14:20:15 +0300

    sync

--- src/eval/hand.rs
+++ src/eval/hand.rs
@@ -31,25 +31,19 @@ fn count_bits(mut flags: u8) -> usize {
     }
     return count as usize;
 }
-// 4 ways to choose 3 bits in a nibble
-fn get_ind_3(flags: u8, rank: u8) -> Vec<u8> {
+// 4 ways to choose 3 bits in a nibble and 6 ways to choose 2
+fn get_suits(flags: u8, rank: u8) -> Vec<u8> {
     match flags {
         14 => vec![card::ind(rank, 3), card::ind(rank, 2), card::ind(rank, 1)],
         13 => vec![card::ind(rank, 3), card::ind(rank, 2), card::ind(rank, 0)],
         11 => vec![card::ind(rank, 3), card::ind(rank, 1), card::ind(rank, 0)],
         7  => vec![card::ind(rank, 2), card::ind(rank, 1), card::ind(rank, 0)],
-    }
-}
-// 6 ways to choose 2 bits in a nibble
-fn get_ind_2(flags: u8, rank: u8) -> Vec<u8> {
-    match flags {
         12 => vec![card::ind(rank, 3), card::ind(rank, 2)],
         10 => vec![card::ind(rank, 3), card::ind(rank, 1)],
         9  => vec![card::ind(rank, 3), card::ind(rank, 0)],
         6  => vec![card::ind(rank, 2), card::ind(rank, 1)],
         5  => vec![card::ind(rank, 2), card::ind(rank, 0)],
         3  => vec![card::ind(rank, 1), card::ind(rank, 0)],
-        _  => get_ind_3(flags, rank),
     }
 }
 
@@ -104,7 +98,7 @@ pub fn full_house(mut flags: u64) -> Option<Showdown> {
     for r in (0..13).rev() {
         let rflags = (0xF & (flags >> r * 4)) as u8;
         if count_bits(rflags) == 3 {
-            cards.extend(get_ind_3(rflags, r));
+            cards.extend(get_suits(rflags, r));
             for i in 0..3 {
                 flags &= !(mask_c(cards[i]));
             }
@@ -114,7 +108,7 @@ pub fn full_house(mut flags: u64) -> Option<Showdown> {
     for r in (0..13).rev() {
         let rflags = (0xF & (flags >> r * 4)) as u8;
         if count_bits(rflags) >= 2 {
-            cards.extend(get_ind_2(rflags, r));
+            cards.extend(get_suits(rflags, r));
             break;
         }
     }
@@ -147,47 +141,14 @@ pub fn flush(flags: u64) -> Option<Showdown> {
 }
 
 pub fn straight(flags: u64) -> Option<Showdown> {
-    // let mut cards: Vec<u8> = Vec::with_capacity(5);
+    let mut cards: Vec<u8> = Vec::with_capacity(5);
+    let mut ranks: u32 = 0;
 
-    // // checks if there's a straight and finds it's rank
-    // for r in 0..13 {
-    //     let mut ranks: u8 = 0;
-    //     for s in 0..4 {
-    //         if (flags & (mask_c(card::ind(r, s)))) != 0 {
-    //             ranks |= ()
-    //         }
-    //     }
-    //     if (flags & mask_r(r)) != 0 {
-
-    //     }
-    // }
-    // // checks for low ace straight
-    // if rank == 0xFF {
-    //     if (flags & 0xF000000000000) != 0
-    //         && (flags & 0x000000000F000) != 0
-    //         && (flags & 0x0000000000F00) != 0
-    //         && (flags & 0x00000000000F0) != 0
-    //         && (flags & 0x000000000000F) != 0
-    //     {
-    //         rank = 3;
-    //     } else {
-    //         return None;
-    //     }
-    // }
-    // assert_ne!(rank, 0xFF);
-    // // finds the cards that form the hand
-    // for r in (rank - 4..=rank).rev() {
-    //     if cards.len() == 5 {
-    //         break;
-    //     }
-    //     for s in (0..4).rev() {
-    //         if (flags & (1 << card::ind(r, s))) != 0 {
-    //             cards.push(card::ind(r, s));
-    //             break;
-    //         }
-    //     }
-    // }
-    // return Some((cards[0], cards[1], cards[2], cards[3], cards[4]));
+    for r in (0..13).rev() {
+        if (flags & (0xF << r)) != 0 {
+            ranks |= 1 << r;
+        }
+    }
     return None;
 }
 
@@ -195,7 +156,7 @@ pub fn three_of_a_kind(mut flags: u64) -> Option<Showdown> {
     for r in (0..13).rev() {
         let rflags = (0xF & (flags >> r * 4)) as u8;
         if count_bits(rflags) == 3 {
-            let cards = get_ind_3(rflags, r);
+            let cards = get_suits(rflags, r);
             flags &= !(mask_c(cards[0]) | mask_c(cards[1]) | mask_c(cards[2]));
             let kickers = find_kickers(flags, 2);
             
